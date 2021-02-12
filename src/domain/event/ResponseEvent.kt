c4b013d705ad8eb507e2b7de0f499b7058e98db1
Kamil <kamilw2000@interia.eu>
2021-02-12 10:32:58 +0100

    + added combined events

--- src/domain/event/ResponseEvent.kt
+++ src/domain/event/ResponseEvent.kt
@@ -10,8 +10,6 @@ sealed class ResponseEvent {
     class NewUser(val user: User) : ResponseEvent()
     class GameStarted(val isStarted: Boolean) : ResponseEvent()
     class Painter(val wordGuess: String): ResponseEvent()
-    //    class Guessing(val wordGuessInUnder: String) : ResponseEvent()
-//    class WordGuess(val userName: String, val wordGuess: String) : ResponseEvent()
     class FirstRoundStarted(val newWordGuess: String, val isPainter: Boolean ) : ResponseEvent()
     class NextRoundStarted(
         val userNameWhoGuessed: String,
